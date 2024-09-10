f89e986c02e6ba42d24d93f219ed944e3f790a06
Лидия бральнина <sateshy77@gmail.com>
2024-09-10 11:58:10 +0300

    добавлет gitignore

---
+++ conhello/conhello/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("имя?");
+string? name = Console.ReadLine();
+Console.WriteLine("город?");
+string? city = Console.ReadLine();
+
+int? age = null;
+
+Console.WriteLine("Имя = "+name +", город = "+ city);
+Console.WriteLine("Имя = {0}, город = {1}", name, city);
+Console.WriteLine($"Имя = {name}, Город = {city}");
+
+Console.WriteLine("город?");
